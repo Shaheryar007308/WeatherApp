98cacbb3b2ff8874bc9ef0c7b7d3bba36b37a0a7
Shaheryar <shaheryar001234@gmail.com>
2026-09-19 20:46:06 +0500

    Initial commit: Completed Weather App

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains:annotations:23.0.0")
+        }
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
